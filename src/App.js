ef0ef95777a8837f1752ec0fcf915ae1d51609ad
= <krishnajha366@gmail.com>
2020-10-18 00:07:36 +0530

    New funtion added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import userEvent from '@testing-library/user-event';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login'
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+
+  const [token, setToken] = useState(null) //Used to handle variables in React. Initially we need to make it null. Volatile memory
+
+  // Runs the code based on condition. We can have multiple useEffects
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = ""   //This is gonna remove the hash token from the URL. Security is IMP 
+
+    const _token = hash.access_token;
+
+    if(_token){
+      setToken(_token)
+    }
+
+  }, [])
+  // Keep the bracket empty if we want to run the useeffect only once,
+  // Or we can mention the variable name based on which it will run everytime the variable value changes
+
   return (
     <div className="App">
-        
-        <Login />
+        {
+          //Ternary operator to check if we have a token, if not then redirect to login page 
+          token ? (
+            // <Player />
+            <h2>I'm Logged In 🔥  </h2>
+          ) : (
+            <Login />
+           )
+        }
+        {/* <Login /> */}
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Login.css'
+import { loginUrl } from './spotify'
 
 function Login() {
     return (
         <div>
             <div className="login">
             {/* Spotify Logo */}
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Spotify_logo_with_text.svg/1200px-Spotify_logo_with_text.svg.png" />
-                <a>Login with Spotify</a>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Spotify_logo_with_text.svg/1200px-Spotify_logo_with_text.svg.png" alt="" />
+                <a href={loginUrl}>Login with Spotify</a>
             </div>
         </div>
     )
